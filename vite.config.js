d008897afc1ae66db0da4d582052305b98ad2b72
haidang321091 <phamhaidang190123@gmail.com>
2024-03-22 16:21:50 +0700

    add xuong html_css

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        shop: resolve(__dirname, "./shop.html"),
+        detail: resolve(__dirname, "./detail.html"),
+      },
+    },
+  },
+});
